e41a7dfc2f3ddf6322e229276f4578c8a6353f5c
Marco Hennermann <79541316+SoftwareRacer@users.noreply.github.com>
2021-04-13 15:31:16 +0200

    Add files via upload

---
+++ constants/Colors.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 13 15:25:30 2021
+
+@author: marco
+"""
+
+primary_colors = {
+    "BLACK": (0, 0, 0),
+    "WHITE": (255, 255, 255),
+    "GRAY": (128, 128, 128),
+}
+
+brick_colors = [
+    (0, 0, 0),
+    (0, 240, 240),  # 4 in einer Reihe
+    (0, 0, 240),  # Reverse L
+    (240, 160, 0),  # L
+    (240, 240, 0),  # Block
+    (0, 240, 0),  # S
+    (160, 0, 240),  # T
+    (240, 0, 0),  # Reverse S
+]
---
+++ constants/GameStates.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 13 15:26:24 2021
+
+@author: marco
+"""
+
+GAME_OVER = "game_over"
+START = "start"
